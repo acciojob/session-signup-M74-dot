39e20d766d143b465072057c5257ea1347b93957
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-09-08 22:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById('signup-form').addEventListener('submit',function(event){
+	event.preventDefault();
+	const name = document.querySelector('#name').value;
+	const email = document.querySelector('#email').value;
+	const password = document.querySelector('#password').value;
+	const cpassword = document.querySelector('#confirm-password').value;
+
+	if(password!==cpassword){
+		alert('Passwords do not match!');
+	}else{
+		sessionStorage.setItem('name',name);
+		sessionStorage.setItem('email',email);
+		sessionStorage.setItem('password',password);
+		alert('Sign up successful!');
+	}
+});
